34c13e4679db9337751b752db4de086b97572e34
Anthoni Zamora <anthoni.zamora@gmail.com>
2025-08-01 18:48:29 -0500

    Evaluacion 1

--- src/components/Ejercicio1.tsx
+++ src/components/Ejercicio1.tsx
@@ -3,25 +3,24 @@ import react from '@vitejs/plugin-react';
 
 const numeros: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
-const Ejercicio1: react.FC = () => {
+const Ejercicio1: React.FC = () => {
     const [pares, setPares] = useState<number[]>([]);
     const [impares, setImpares] = useState<number[]>([]);
     const [clasificado, setClasificado] = useState(false);
 
     const handleClasificar = () => {
-        setPares(numeros, filter(n => n % 2 === 0));
+        setPares(numeros.filter(n => n % 2 === 0));
         setImpares(numeros.filter(n => n % 2 !== 0));
         setClasificado(true);
-    }
+    };
+
     return (
         <div className="text-center mt-6">
             <h2 className="text-x1 font-bold text-blue-400">Bienvenido</h2>
             <p className="mt-2">
                 <strong>Vector: </strong>
             </p>
-            <button>
-
-            </button>
+            <button onClick={handleClasificar} className="mt-4 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">Clasificar</button>
         </div>
     )
 }
