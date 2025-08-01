384963d98c5b0403a7d2642e02705ad3496428b6
Anthoni Zamora <anthoni.zamora@gmail.com>
2025-08-01 18:36:57 -0500

    Evaluacion 1

---
+++ src/components/Ejercicio1.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Ejercicio1 = () => {
+  return (
+    <div className="text-center mt-6">
+      <h2 className="text-x1 font-bold text-blue-400">Bienvenido</h2>
+      <p className="mt-2">
+        <strong>Vector: </strong>
+      </p>
+      <button>
+
+      </button>
+    </div>
+  )
+}
+
+export default Ejercicio1
